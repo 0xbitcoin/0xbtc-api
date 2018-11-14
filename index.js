2c79a79d6f0d4fb83ae9dd999115df1db6ce75b7
andy <andy@jonezie.com>
2018-11-13 21:58:26 -0500

    coming along

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var EthHelper = require ('./lib/ethhelper')
+
+
+//var mongoInterface = require('./lib/mongo-interface')
+
+function init()
+{
+  console.log('Booting 0xBTC API bot.')
+  var ethHelper = new EthHelper();
+
+
+  //start express
+
+}
+
+
+
+init();
