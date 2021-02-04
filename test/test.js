4381611f1c3c444d724aa118b7444ade1b283db6
Andy <admazzola@gmail.com>
2021-02-04 14:39:17 -0500

    test passes

--- test/test.js
+++ test/test.js
@@ -2,8 +2,13 @@
 import Web3Plug from '../lib/js/web3plug' 
 import EIP918Token from '../lib/js/eip918token' 
 
+import chai from 'chai';
 
-var assert = require('assert');
+chai.config.includeStack = true;
+chai.use(require('chai-as-promised'))
+  
+var assert = chai.assert
+ 
 
 const Web3 = require('web3')
 
@@ -13,23 +18,33 @@ const botconfig = require('../bot.config.json')
 
 var web3Plug = new Web3Plug(botconfig.web3provider);
 
-
-describe('web3api', async function() {
   
-    var web3 = web3Plug.getWeb3()
 
-    let networkId = await web3Plug.getNetworkId()
-
-    let zxbtcContractAddress = web3Plug.getContractDataForNetworkID(networkId)
-
-    var zxbtcToken = new EIP918Token(web3, zxbtcContractAddress)
+describe('web3api',  () => {
+  
 
 
+        it('Should api stuff', async () => {
 
+            var web3 = web3Plug.getWeb3()
 
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+            let networkId = await web3Plug.getNetworkId()
+
+            console.log('networkId',networkId) 
+
+            let zxbtcContractAddress = web3Plug.getContractDataForNetworkID(networkId)._0xbitcointoken.address
+
+            console.log('zxbtcContractAddress',zxbtcContractAddress)
+
+            var zxbtcToken = new EIP918Token(web3, zxbtcContractAddress)
+            
+            console.log(zxbtcToken.getContract())
+
+            let difficulty = await zxbtcToken.getContract().methods.getMiningDifficulty().call()
+               console.log('difficulty',difficulty)
+        
+                assert.equal(parseInt(difficulty), 1519206521);
+            }); 
 
 
 
